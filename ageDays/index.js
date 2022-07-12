4c92efa2766cd3eee75a5ba60dca17c479047aee
FrApps <54560710+FranklinD10@users.noreply.github.com>
2022-07-12 16:47:14 +0000

    v1.1.7

--- ageDays/index.js
+++ ageDays/index.js
@@ -4,25 +4,31 @@ var myAge;
 var humanDogRatio;
 
 function execute() { 
-    print();
+
+ userAge = getElementById('age').value;
+
 }
 
 function calculate() {
-    myAge = userAge
-    humanDogRatio = 8
 
-    count = myAge * humanDogRatio
+    myAge = userAge;
+    humanDogRatio = 7;
+
+    count = myAge * humanDogRatio;
 }
 
 function check() {
+
    if (userAge == null) {
     
     } 
 }
 
 function print() {
+
     document.getElementById("content-title").innerText = "Results:";
-    document.getElementById("content-age").innerHTML = count;
+    document.getElementById("content-results").innerText = count;
+    
 }
 
 export { execute, calculate, check, print, count, userAge, myAge, humanDogRatio };
